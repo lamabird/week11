19a9d56ed368a3db36f563c0ea350f228501e085
liam <liamwilsonliamwilson@gmail.com>
2023-03-20 14:52:41 -0700

    function practice 2

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
-// function greet(name) {
-//     console.log('hello ${name}');
-//
-//
-// }
+function greet(name, time = "day") {
+    console.log('good ${time} ${name}');
+}
+
+greet("tim", "night")
 
 // let greet = function (name) {
 //     console.log('hello $(name)');
@@ -14,3 +14,15 @@
 //     console.log('hello $(name)');
 // }
 // greet("tim")
+function circlearea(radius) {
+    if (isNaN(radius)) {
+        alert("this is not a string");
+    } else {
+        let area = Math.PI * radius * radius;
+        return area
+    }
+}
+
+let uservalue = prompt("enter a number");
+let result = circlearea(<input type="">)
+alert('the are of a circle with radius ${userValue} is ${result}')
