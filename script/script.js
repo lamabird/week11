d45aa6ac8371e3f7116d92cd0c653d6df60fd9b8
liam <liamwilsonliamwilson@gmail.com>
2023-03-20 14:25:19 -0700

    javascript function practice

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// function greet(name) {
+//     console.log('hello ${name}');
+//
+//
+// }
+
+// let greet = function (name) {
+//     console.log('hello $(name)');
+// }
+//
+// greet("tim")
+
+// let greet = (name) => {
+//     console.log('hello $(name)');
+// }
+// greet("tim")
