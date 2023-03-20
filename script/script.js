146cc0d6fc4e620e224bbb587165954a36b7350c
liam <liamwilsonliamwilson@gmail.com>
2023-03-20 16:20:02 -0700

    function practice 4

--- script/script.js
+++ script/script.js
@@ -1,38 +1,57 @@
-// function greet(name, time = "day") {
-//     console.log('good ${time} ${name}');
+// // function greet(name, time = "day") {
+// //     console.log('good ${time} ${name}');
+// // }
+// //
+// // greet("tim", "night")
+//
+// // let greet = function (name) {
+// //     console.log('hello $(name)');
+// // }
+// //
+// // greet("tim")
+//
+// // let greet = (name) => {
+// //     console.log('hello $(name)');
+// // }
+// // greet("tim")
+//
+// function circlearea(radius) {
+//     if (isNaN(radius)) {
+//         alert("this is not a string");
+//     } else {
+//         let area = Math.PI * radius * radius;
+//         return area
+//     }
 // }
 //
-// greet("tim", "night")
-
-// let greet = function (name) {
-//     console.log('hello $(name)');
-// }
+// let userValue = prompt("enter a number");
+// let result = circlearea(<input type="text"/>)
+// alert = ('the area of a circle with radius ${userValue} is ${result}');
+// document.querySelector("#radius")
+// console.log(RadiusPara);
+// document.querySelector("#result")
+// console.log(ResultPara)
+// RadiusPara.textContent = userValue;
+// let result = circlearea(userValue)
+// if (result !== undefined) {
 //
-// greet("tim")
-
-// let greet = (name) => {
-//     console.log('hello $(name)');
 // }
-// greet("tim")
+const ulList = document.querySelector(".shopping")
 
-function circlearea(radius) {
-    if (isNaN(radius)) {
-        alert("this is not a string");
-    } else {
-        let area = Math.PI * radius * radius;
-        return area
+function populatelist(shoppinglistarray) {
+    for (let item of shoppinglistarray) {
+        const newli = document.createElement("li")
+        newli.textContent = item;
+        ulList.appendChild(newli)
+
     }
+
 }
 
-let userValue = prompt("enter a number");
-let result = circlearea(<input type="text"/>)
-alert = ('the area of a circle with radius ${userValue} is ${result}');
-document.querySelector("#radius")
-console.log(RadiusPara);
-document.querySelector("#result")
-console.log(ResultPara)
-RadiusPara.textContent = userValue;
-let result = circlearea(userValue)
-if (result !== undefined) {
+let myshoppinglist = ["cheese", "bread", "green pepper"];
+populatelist(myshoppinglist);
 
+function squarlistmaker() {
+    ulList.classList.add("squarelist")
+    ulList.classList.remove("circlelist")
 }
